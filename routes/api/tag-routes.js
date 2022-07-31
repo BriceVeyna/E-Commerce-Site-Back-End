8ccbb53c8c1796c2645ba74efd7ca0aa83529a55
Brice Veyna <bricev99@gmail.com>
2022-07-31 08:34:22 -0700

    Fix errors

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -20,7 +20,7 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const tagData = await Tag.findByPk({
+    const tagData = await Tag.findByPk(req.params.id, {
       include: [{ model: Product }],
     });
 
@@ -68,7 +68,7 @@ router.delete('/:id', async (req, res) => {
       req.status(404).json({ message: 'No tag found with that id' });
       return;
     }
-    
+
     res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
